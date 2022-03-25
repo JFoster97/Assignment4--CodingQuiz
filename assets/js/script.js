8e5f95afdfccaf5fcca99f474eb1a2b5cf18380c
Jonah Foster <jonahfoster97@gmail.com>
2022-03-25 13:22:26 -0500

    cannot get the questions to display, timer is functional though

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,136 @@
-const questionsArray = [question1, question2, question3, question4, question5];
+const questionsArray = [
+  {
+    question: "Inside which HTML element do we link the JavaScript file?",
+    answer1: "1. <javascript>",
+    answer2: "2. <script>",
+    answer3: "3. <js>",
+    answer4: "4. <sripting>",
+    correct: "2. <script>"
+  },
+  {
+    question: "What must the conditional statement of and if/else be enclosed within?",
+    answer1: "1. quotes",
+    answer2: "2. curly brackets",
+    answer3: "3. parentheses",
+    answer4: "4. square brackets",
+    correct: "3. parentheses"
+},
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    answer1: "1. alert('Hello World')",
+    answer2: "2. msg('Hello World')",
+    answer3: "3. msgBox('Hello World')",
+    answer4: "4. alertBox('HelloWorld')",
+    correct: "1. alert('Hello World')"
+  },
+  {
+    question: "What is the correct syntax for creating a function in JavaScript?",
+    answer1: "1. function = myFunction()",
+    answer2: "2. function myFunction()",
+    answer3: "3. function:myFunction()",
+    answer4: "4. var myFunction()",
+    correct: "2. function myFunction()"
+  },
+  {
+    question: "What is the correct way to call a function in JavaScript?",
+    answer1: "1. call myFunction()",
+    answer2: "2. call function myFunction()",
+    answer3: "3. myFunction()",
+    answer4: "4. use myFunction()",
+    correct: "3. myFunction()"
+  }
+]
 
-var question1 = "<h2>Inside which HTML element do we link the JavaScript file?</h2> <ul> <li><p>javascript</p><button type='button' class = 'btn btn-primary btn-sm answerBtn'></button>Select</li> <li><p>js</p><<button type='button' class = 'btn btn-primary btn-sm answerBtn'>>Select</button></li> <li><p>script</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id='correct'>Select</button></li> <li><p>scripting</p><button type='button' class = 'btn btn-primary btn-sm answerBtn'>Select</button></li> </ul>";
+var a1 = $('#a1')
+var a2 = $('#a2')
+var a3 = $('#a3')
+var a4 = $('#a4')
 
-var question2 = "<h2>Where in the DOM is the correct place to insert a JavaScript link</h2> <ul> <li><p>The head section</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id='correct'></button></li> <li><p>The body section</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id='correct'></button></li> <li><p>Both the head and the body section</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id='correct'></button></li> </ul>"
+var timeCount
 
-var question3 = "<h2>How do you write 'Hello World' in an alert box?</h2> <ul> <li><p>alert('Hello World');</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id='correct'></button></li> <li><p> msg('Hello World');</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' ></button></li> <li><p>msgBox('Hello World');<button type='button' class = 'btn btn-primary btn-sm answerBtn'></button></li> <li><p>alertBox('HelloWorld');<button type='button' class = 'btn btn-primary btn-sm answerBtn'></button></li> </ul>"
+var startingTime = 75
 
-var question4 = "<h2>What is the correct syntax for creating a function in JavaScript?</h2> <ul> <li><p>function = myFunction()</p><button type='button' class = 'btn btn-primary btn-sm answerBtn'></button></li> <li><p>function myFunction()</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' id = 'correct'></button></li> <li><p>function:myFunction()</p><button type='button' class = 'btn btn-primary btn-sm answerBtn' ></button></li> </ul>"
+var timerEl = $('#timer')
 
-var question5 = "<h2>What is the correct way to call a function in JavaScript?</h2> <ul> <li><p>call myFunction()</p><button></button></li> <li><p> call function myFunction()</p><button></button></li> <li><p>myFunction()</p><button></button></li> </ul>"
+var answerBtn = $('.answerBtn')
 
-var timeLoss = 10
+var complete = false
 
-var score =
+var questionIndex = 0
 
-const randomQuestion = questionsArray[Math.floor(Math.random() * Array.length)];
+var randomQuestion = questionsArray[Math.floor(Math.random() * Array.length)];
+
+
 
 $('.startBtn').on("click", function() {
   quizStart();
+  console.log('the quiz has begun')
 })
 
 function quizStart() {
-  $('.questionsBox').append(randomQuestion);
-  $('.answerBtn').on('click', function() {
-    if (id==='correct') {
-      alert('Correct');
-      $('.questionsBox').clear();
-      $('.questionsBox').append(randomQuestion);
-    } else {
-      alert("Incorrect, try again")
+  showQuestion();
+  startTimer();
+}
 
-    }
-  })
+function showQuestion() {
+
+  $('.question').textContent = questionsArray[questionIndex].question;
+  a1.textContent = questionsArray[questionIndex].answer1;
+  a2.textContent = questionsArray[questionIndex].answer2;
+  a3.textContent = questionsArray[questionIndex].answer3;
+  a4.textContent = questionsArray[questionIndex].answer4;
+}
+
+function checkAnswer(choice) {
+  if(choice !== questionsArray[questionIndex].correct) {
+    timeCount-=10;
+    alert("Incorrect, try again")
+  }else{
+    alert("Correct!")
+  }
+
+  questionIndex++;
+  if (questionIndex >= questionsArray.length) {
+    finishQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+function finishQuiz() {
+
+}
+
+function startTimer() {
+    timeCount = startingTime
+    timerEl.textContent = timerFunction(timeCount)
+    
+    timer = setInterval(function() {
+        timeCount--
+    
+        if (timeCount <= 0) {
+            clearInterval(timer)
+            scorePage()
+        } else {
+            timerEl.textContent = timerFunction(timeCount)
+        }
+    
+    }, 1000)
+}
+
+function timerFunction(seconds) {
+  let m = Math.floor(seconds / 60).toString().padStart(2, 1)
+  let s = (seconds % 60).toString().padStart(2, '0')
+    return `${m}:${s}` 
+}
+
+for(i of answerBtn) {
+
+  i.addEventListener('click', function() {
+      checkAnswer(this.innerHTML);
+    });
+}
+
+function scorePage() {
+  alert('Your score is' + timeCount);
 }
